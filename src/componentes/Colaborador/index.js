6eab4f3b67c45d9240621c8cfe67fe0ba130d3c0
Gianina Gaete <gianinagaete@gmail.com>
2025-01-16 18:46:52 -0300

    fin

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,21 @@
 import "./Colaborador.css"
+import { IoMdCloseCircle, IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 
-const Colaborador = () => {
+
+const Colaborador = (props) => {
+  const { nombre, puesto, foto, equipo, id, fav }= props.datos
+  const { colorPrimario, eliminarColaborador, like }= props
   return <div className="colaborador">
-    <div className="encabezado">
-      <img src="https://github.com/NinaGaZu.png" alt="Nina" />
+    <IoMdCloseCircle className="eliminar" onClick={() => eliminarColaborador(id)} />
+    <div className="encabezado" style={{backgroundColor:colorPrimario}}>
+      <img src={foto} alt={nombre} />
     </div>
     <div className="info">
-      <h4>Nina</h4>
-      <h5>Instructor</h5>
+      <h4>{nombre}</h4>
+      <h5>{puesto}</h5>
+      {fav ? <IoMdHeart color="red" onClick={() => like(id)} /> : <IoMdHeartEmpty onClick={() => like(id)} /> }
+      
+      
     </div>
   </div>
 }
